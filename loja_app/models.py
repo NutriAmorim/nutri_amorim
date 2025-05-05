d15c4da9dbf7b721d9ce0987e7a25db5c32963e5
Marcos Felipe Amorim Mendes <nutriamorim69@gmail.com>
2025-05-04 23:18:38 -0300

    adicionando alterações sitemaps

--- loja_app/models.py
+++ loja_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 
+
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
