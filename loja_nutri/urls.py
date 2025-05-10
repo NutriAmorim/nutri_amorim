821d5190946183d54dfc7db3e2339b8c66a02ab9
Marcos Felipe Amorim Mendes <nutriamorim69@gmail.com>
2025-05-10 13:23:47 -0300

    adicionando sitemaps

--- loja_nutri/urls.py
+++ loja_nutri/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
     path('', include('loja_app.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
