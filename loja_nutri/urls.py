e47fca236bd6d97032dbf4f0504e679ddb8899f7
Marcos Felipe Amorim Mendes <nutriamorim69@gmail.com>
2025-05-10 18:35:58 -0300

    adicionand o robots.txt

--- loja_nutri/urls.py
+++ loja_nutri/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StaticViewSitemap
+from django.views.generic import TemplateView
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -12,4 +13,5 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('', include('loja_app.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
